85402a72db74212b56424f9669c2d7b4f9bc9152
Neil McAlister <neil.mcalister@futurice.com>
2019-01-18 11:32:04 +0200

    Clean up some unused things

--- ClientApp/src/components/StartPage.tsx
+++ ClientApp/src/components/StartPage.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
 import "./StartPage.scss";
 import "./Common.scss";
 import { NetworkService } from "../services/NetworkService";
